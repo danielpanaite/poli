eb3c1fbb89271c0957f3bd9d16fa422bb8e7c5d4
danielpanaite <dani.panaite@gmail.com>
2022-04-12 10:26:38 +0200

    rust tratti

--- rust/tratti/src/main.rs
+++ rust/tratti/src/main.rs
@@ -1,5 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+struct Pasto{
+    name: String,
+    cibo: String,
+}
 
-    
+trait Mangiare{
+    fn cibo(&mut self); 
+}
+
+impl Mangiare for Pasto{
+    fn cibo(&mut self){
+        self.cibo = "Mela".to_string();
+    }
+}
+
+fn main() {
+
+    let mut pranzo = Pasto{ name:"Pranzo".to_string(), cibo:"".to_string() };
+    pranzo.cibo();
+    println!("{}", pranzo.cibo);
+
 }
