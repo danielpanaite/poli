98d66209486532ebfac1bc541cd98d548814230c
danielpanaite <dani.panaite@gmail.com>
2022-06-19 00:29:08 +0200

    changed server handle

--- rust/groupchat/src/client.rs
+++ rust/groupchat/src/client.rs
@@ -3,13 +3,19 @@ use std::net::{TcpStream, Shutdown};
 
 pub fn send_to_server(address: String, message: &str) -> std::io::Result<String> {
     let mut stream = TcpStream::connect(address)?;
-
-    stream.write(message.as_bytes()).expect("Response send failed");
-    stream.shutdown(Shutdown::Write).unwrap();
-
+    let mut s = String::new();
     let mut reply = String::new();
-    stream.read_to_string(&mut reply).expect("Failed reading server response");
-    println!("Server reply: {}", reply);
-
+    loop{
+        
+        if std::io::stdin().read_line(&mut s).is_ok(){
+            println!("Readline ok");
+        }
+        stream.write(s.as_bytes()).expect("Response send failed");
+        //stream.shutdown(Shutdown::Write).unwrap();
+        
+        stream.read_to_string(&mut reply).expect("Failed reading server response");
+        println!("Server reply: {}", reply);
+    }
+    
     Ok(reply)
 }
--- rust/groupchat/src/server.rs
+++ rust/groupchat/src/server.rs
@@ -1,5 +1,7 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::net::{TcpStream, TcpListener};
+use std::io::prelude::*;
+//use std::io::{BufRead, BufReader, BufWriter};
+use std::io::Write;
+use std::net::{TcpStream, TcpListener, Shutdown};
 
 pub fn start_server() -> std::io::Result<()> {
 
@@ -8,7 +10,7 @@ pub fn start_server() -> std::io::Result<()> {
     for stream in listener.incoming(){
         match stream{
             Ok(stream) => {
-                handle_connection(stream).unwrap();
+                handle_connection(stream);
             }
             Err(e) => { println!("Accept failed: {:?}", e); }
         }
@@ -17,20 +19,39 @@ pub fn start_server() -> std::io::Result<()> {
     Ok(())
 }
 
-fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
-
-    let mut br: BufReader<&TcpStream> = BufReader::new(&stream);
-    let mut bw: BufWriter<&TcpStream> = BufWriter::new(&stream);
-    let mut s = String::new();
-    loop{
-        let size = br.read_line(&mut s)?;
-        if size == 0 { return Ok(()); }
-
-        s = "Welcome, ".to_string() + &s + "!";
-
-        bw.write(s.as_bytes())?;
-        bw.flush().unwrap();
-        s.clear();
-    }
-    
+fn handle_connection(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            for i in 0..size{
+                print!("{}", data[i] as char);
+            }
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
 }
+
+// fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
+
+//     let mut br: BufReader<&TcpStream> = BufReader::new(&stream);
+//     let mut bw: BufWriter<&TcpStream> = BufWriter::new(&stream);
+//     let mut s = String::new();
+//     loop{
+//         let size = br.read_line(&mut s)?;
+//         if size == 0 { return Ok(()); }
+
+//         s = "Welcome, ".to_string() + &s + "!";
+
+//         bw.write(s.as_bytes())?;
+//         bw.flush().unwrap();
+//         s.clear();
+//     }
+    
+// }
